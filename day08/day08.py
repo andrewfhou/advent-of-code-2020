e1d4ef56a549be97ad65ca64cdf73dfd119e63ea
Andrew Hou <andrewfhou@gmail.com>
2020-12-08 00:31:15 -0500

    day08 solved

--- day08/day08.py
+++ day08/day08.py
@@ -1,4 +1,5 @@
 import time
+import copy
 
 CURR_MS = lambda: time.time() * 1000
 
@@ -9,14 +10,43 @@ print('+-------------------------+')
 START_READ = CURR_MS()
 print('\nREADING FILE... ',end='')
 with open("input.txt") as file:
-    inputs = file.read().strip().split()
+    inputs = file.read().strip().split('\n')
 print('%.6fms\n' % (CURR_MS() - START_READ))
 
 def part_one():
-    return 0
+    return run(inputs)[1]
 
 def part_two():
-    return 0
+    for lnum in range(len(inputs)):
+        instr = inputs[lnum].split(' ')[0]
+        if instr == 'nop':
+            mod = copy.deepcopy(inputs)
+            mod[lnum] = mod[lnum].replace('nop', 'jmp')
+            ret = run(mod)
+            if ret[0] != -1: return ret[1]
+        elif instr == 'jmp':
+            mod = copy.deepcopy(inputs)
+            mod[lnum] = mod[lnum].replace('jmp', 'nop')
+            ret = run(mod)
+            if ret[0] != -1: return ret[1]
+
+def run(code):
+    acc = 0
+    lnum = 0
+    visited = set()
+    while True:
+        if lnum >= len(code): return [1, acc]
+        line = code[lnum]
+        if lnum in visited: return [-1, acc]
+        visited.add(lnum)
+        instr, arg = line.split(' ')
+        if instr == 'acc':
+            acc += int(arg)
+            lnum += 1
+        elif instr == 'jmp':
+            lnum += int(arg)
+        elif instr == 'nop':
+            lnum += 1
 
 START_ONE = CURR_MS()
 print('PART ONE: ' + str(part_one()))
