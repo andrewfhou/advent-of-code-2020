8cbb4d8ac24ef7eb6ebf6c2898a10c61b206c299
Andrew Hou <andrewfhou@gmail.com>
2020-12-09 00:32:16 -0500

    setup day10

---
+++ day10/day10.py
@@ -0,0 +1,28 @@
+import time
+
+CURR_MS = lambda: time.time() * 1000
+
+print('+-------------------------+')
+print('| ADVENT OF CODE - DAY XX |')
+print('+-------------------------+')
+
+START_READ = CURR_MS()
+print('\nREADING FILE... ',end='')
+with open("input.txt") as file:
+# with open("example.txt") as file:
+    inputs = file.read().strip().split()
+print('%.6fms\n' % (CURR_MS() - START_READ))
+
+def part_one():
+    return 0
+
+def part_two():
+    return 0
+
+START_ONE = CURR_MS()
+print('PART ONE: ' + str(part_one()))
+print('TIME TAKEN... %.6fms\n' % (CURR_MS() - START_ONE))
+
+START_TWO = CURR_MS()
+print('PART TWO: ' + str(part_two()))
+print('TIME TAKEN... %.6fms\n' % (CURR_MS() - START_TWO))
