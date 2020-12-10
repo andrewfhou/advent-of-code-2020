a2c6499633e63bbf987825c187587752527828b1
Andrew Hou <andrewfhou@gmail.com>
2020-12-10 00:37:42 -0500

    day10 solved

--- day10/day10.py
+++ day10/day10.py
@@ -3,7 +3,7 @@ import time
 CURR_MS = lambda: time.time() * 1000
 
 print('+-------------------------+')
-print('| ADVENT OF CODE - DAY XX |')
+print('| ADVENT OF CODE - DAY 10 |')
 print('+-------------------------+')
 
 START_READ = CURR_MS()
@@ -11,13 +11,29 @@ print('\nREADING FILE... ',end='')
 with open("input.txt") as file:
 # with open("example.txt") as file:
     inputs = file.read().strip().split()
+    inputs = [int(x) for x in inputs]
+    inputs.append(0)
+    inputs.append(max(inputs) + 3)
+    inputs.sort()
 print('%.6fms\n' % (CURR_MS() - START_READ))
 
 def part_one():
-    return 0
+    delt1 = 0
+    delt3 = 0
+    for i in range(len(inputs) - 1):
+        delta = inputs[i + 1] - inputs[i]
+        if delta == 1: delt1 += 1
+        else: delt3 += 1
+    return delt1 * delt3
 
 def part_two():
-    return 0
+    count = [0] * len(inputs)
+    count[0] = 1
+    for i in range(len(inputs)):
+        for j in range(0, i):
+            if inputs[i] - inputs[j] <= 3:
+                count[i] += count[j]
+    return count[-1]
 
 START_ONE = CURR_MS()
 print('PART ONE: ' + str(part_one()))
