e1536621105533ad065b4bdf43320902933b4d19
Andrew Hou <andrewfhou@gmail.com>
2020-12-12 23:00:09 -0600

    day12 solved

--- day12/day12.py
+++ day12/day12.py
@@ -3,7 +3,7 @@ import time
 CURR_MS = lambda: time.time() * 1000
 
 print('+-------------------------+')
-print('| ADVENT OF CODE - DAY 07 |')
+print('| ADVENT OF CODE - DAY 12 |')
 print('+-------------------------+')
 
 START_READ = CURR_MS()
@@ -13,11 +13,63 @@ with open("input.txt") as file:
     inputs = file.read().strip().split()
 print('%.6fms\n' % (CURR_MS() - START_READ))
 
+dirs = {0 : 'E', 90 : 'N', 180 : 'W', 270 : 'S'}
+
 def part_one():
-    return 0
+    x = y = curr_dir = 0
+
+    for instr in inputs:
+        op = instr[0]
+        val = int(instr[1:])
+
+        if op == 'L':
+            curr_dir = (curr_dir + val) % 360
+        elif op == 'R':
+            curr_dir = (curr_dir - val) % 360
+        elif op == 'F':
+            x, y = travel(x, y, dirs[curr_dir], val)
+        else:
+            x, y = travel(x, y, op, val)
+    return abs(x) + abs(y)
+
+def travel(x, y, op, val):
+    if op == 'N':
+        y += val
+    elif op == 'S':
+        y -= val
+    elif op == 'E':
+        x += val
+    elif op == 'W':
+        x -= val
+    return x, y
 
 def part_two():
-    return 0
+    x_wp = 10
+    y_wp = 1
+    x = y = 0
+
+    for instr in inputs:
+        op = instr[0]
+        val = int(instr[1:])
+
+        if op == 'L':
+            x_wp, y_wp = rotate_wp(x_wp, y_wp, val % 360)
+        elif op == 'R':
+            x_wp, y_wp = rotate_wp(x_wp, y_wp, (-val) % 360)
+        elif op == 'F':
+            x += x_wp * val
+            y += y_wp * val
+        else:
+            x_wp, y_wp = travel(x_wp, y_wp, op, val)
+    return abs(x) + abs(y)
+
+def rotate_wp(x, y, rot):
+    if rot == 90:
+        return -y, x
+    elif rot == 180:
+        return -x, -y
+    elif rot == 270:
+        return y, -x
 
 START_ONE = CURR_MS()
 print('PART ONE: ' + str(part_one()))
