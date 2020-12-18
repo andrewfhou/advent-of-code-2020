2e47927ef9c8d3f6c6807c7e9fb8c0afbe455430
Andrew Hou <andrewfhou@gmail.com>
2020-12-18 09:00:59 -0600

    day18 initial solve

--- day18/day18.py
+++ day18/day18.py
@@ -1,4 +1,5 @@
 import time
+import re
 
 CURR_MS = lambda: time.time() * 1000
 
@@ -8,16 +9,56 @@ print('+-------------------------+')
 
 START_READ = CURR_MS()
 print('\nREADING FILE... ',end='')
-# with open("input.txt") as file:
-with open("example.txt") as file:
-    inputs = file.read().strip().split()
+with open("input.txt") as file:
+# with open("example.txt") as file:
+    inputs = file.read().replace(' ', '').split('\n')[:-1]
 print('%.6fms\n' % (CURR_MS() - START_READ))
 
 def part_one():
-    return 0
+    total = 0
+    for expr in inputs:
+        total += calc(expr, 0, len(expr))
+    return total
+
+def calc(expr, start, end):
+    curr = start
+    soln = 0
+    add = True
+
+    while curr < end:
+        if expr[curr] == '+':
+            add = True
+        elif expr[curr] == '*':
+            add = False
+        elif expr[curr] == '(':
+            parens = 1
+            paren_close = curr + 1
+            while parens > 0:
+                if expr[paren_close] == ')':
+                    parens -= 1
+                elif expr[paren_close] == '(':
+                    parens += 1
+                paren_close += 1
+            paren_close -= 1
+
+            if add: soln += calc(expr, curr + 1, paren_close)
+            else: soln *= calc(expr, curr + 1, paren_close)
+            curr = paren_close
+        else:
+            if add: soln += int(expr[curr])
+            else: soln *= int(expr[curr])
+        curr += 1
+    return soln
+
+class P2(int):
+    def __add__(self, x):
+        return P2(int.__mul__(self, x))
+    def __mul__(self, x):
+        return P2(int.__add__(self, x))
 
 def part_two():
-    return 0
+    exprs = [x.replace('+', '|').replace('*', '+').replace('|', '*') for x in inputs]
+    return sum([eval(re.sub(r'(\d+)', r'P2(\1)', x)) for x in exprs])
 
 START_ONE = CURR_MS()
 print('PART ONE: ' + str(part_one()))
