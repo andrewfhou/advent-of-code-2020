833a506eed2ffa0bf181e50791e8b7707956f09c
Andrew Hou <andrewfhou@gmail.com>
2020-12-20 00:49:17 -0600

    this chall is awful all ive done is parsed the input barely not really

--- day20/day20.py
+++ day20/day20.py
@@ -8,12 +8,16 @@ print('+-------------------------+')
 
 START_READ = CURR_MS()
 print('\nREADING FILE... ',end='')
-with open("input.txt") as file:
-# with open("example.txt") as file:
-    inputs = file.read().strip().split()
+# with open("input.txt") as file:
+with open("example.txt") as file:
+    inputs = file.read().strip().split('\n\n')
+    tiles = {}
+    for tile in inputs:
+        tiles[int(tile.split('\n')[0].split(' ')[1][:-1])] = tile.split('\n')[1:]
 print('%.6fms\n' % (CURR_MS() - START_READ))
 
 def part_one():
+    print(tiles)
     return 0
 
 def part_two():
