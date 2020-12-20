2ff74025cd6b2bedc8976097471e69db275250d9
Andrew Hou <andrewfhou@gmail.com>
2020-12-20 12:46:04 -0600

    day20 pt1 solved

--- day20/day20.py
+++ day20/day20.py
@@ -1,4 +1,5 @@
 import time
+import itertools
 
 CURR_MS = lambda: time.time() * 1000
 
@@ -8,17 +9,41 @@ print('+-------------------------+')
 
 START_READ = CURR_MS()
 print('\nREADING FILE... ',end='')
-# with open("input.txt") as file:
-with open("example.txt") as file:
+with open("input.txt") as file:
+# with open("example.txt") as file:
     inputs = file.read().strip().split('\n\n')
-    tiles = {}
-    for tile in inputs:
-        tiles[int(tile.split('\n')[0].split(' ')[1][:-1])] = tile.split('\n')[1:]
 print('%.6fms\n' % (CURR_MS() - START_READ))
 
 def part_one():
-    print(tiles)
-    return 0
+    edges = []
+    tiles = []
+    for x in range(len(inputs)):
+        e = find_edges(inputs[x].split('\n'))
+        edges.append(e)
+        tiles.append(e)
+
+    soln = 1
+    for tile in tiles:
+        uniques = 0
+        for edge in tile[1:]:
+            if list(itertools.chain(*tiles)).count(edge) == 1:
+                uniques += 1
+        if uniques == 4: # 4 uniques means 2 unique edges (fwd/rev)
+            soln *= int(tile[0].split(' ')[1][:-1])
+    return soln
+
+def find_edges(t):
+    return [
+            t[0],                                  # tile #
+            t[1],                                  # N; W -> E
+            t[1][::-1],                            # N; E -> W
+            t[-1],                                 # S; W -> E
+            t[-1][::-1],                           # S; E -> W
+            ''.join([x[0] for x in t[1:]]),        # W; N -> S
+            ''.join([x[0] for x in t[1:]])[::-1],  # W; S -> N
+            ''.join([x[-1] for x in t[1:]]),       # E; N -> S
+            ''.join([x[-1] for x  in t[1:]])[::-1] # E; S -> N
+    ]
 
 def part_two():
     return 0
