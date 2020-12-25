4bbc1747c567a96c14945c963fdc7062fee5d48d
Andrew Hou <andrewfhou@gmail.com>
2020-12-24 23:30:55 -0600

    day25 solved

--- day25/day25.py
+++ day25/day25.py
@@ -3,7 +3,7 @@ import time
 CURR_MS = lambda: time.time() * 1000
 
 print('+-------------------------+')
-print('| ADVENT OF CODE - DAY XX |')
+print('| ADVENT OF CODE - DAY 25 |')
 print('+-------------------------+')
 
 START_READ = CURR_MS()
@@ -14,10 +14,27 @@ with open("input.txt") as file:
 print('%.6fms\n' % (CURR_MS() - START_READ))
 
 def part_one():
-    return 0
+    return find_key(int(inputs[0]), find_loop(int(inputs[1])))
+
+def find_loop(pub_key):
+    loop = 0
+    res = 1
+    while True:
+        loop += 1
+        res *= 7
+        res %= 20201227
+        if res == pub_key:
+            return loop
+
+def find_key(subj, loop):
+    find_loop_key = 1
+    for x in range(loop):
+        find_loop_key = find_loop_key * subj
+        find_loop_key = find_loop_key % 20201227
+    return find_loop_key
 
 def part_two():
-    return 0
+    return 'done!'
 
 START_ONE = CURR_MS()
 print('PART ONE: ' + str(part_one()))
